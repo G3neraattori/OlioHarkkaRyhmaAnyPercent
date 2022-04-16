db98010c0bad57bf7828b065246ca05c0e1351e3
BuildTools <unconfigured@null.spigotmc.org>
2022-04-16 16:54:34 +0300

    asdf

--- app/src/main/java/com/example/olioharkkaryhmaanypercent/MainActivity.java
+++ app/src/main/java/com/example/olioharkkaryhmaanypercent/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.olioharkkaryhmaanypercent;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
 import java.sql.Time;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity { // asdf
 
@@ -22,11 +29,11 @@ public class MainActivity extends AppCompatActivity { // asdf
 
         // For testing class construction
         movieManager.entryList.add(new Entry("12:00", "12.04.2022"));
-        movieManager.entryList.get(0).entryMovie = new Movie("Yön ritari", "Batman ja muut seikailee",
-                new Cast("Nintentovehkeet", new ArrayList<String>()), 20000){};
+        movieManager.entryList.get(0).entryMovie = new Movie("Yön ritari", 123, "Batman ja muut seikailee",
+                new Cast("Nintentovehkeet", new ArrayList<>()), 20000){};
         movieManager.entryList.add(new Entry("12:00", "13.04.2022"));
-        movieManager.entryList.get(1).entryMovie = new Movie("yokeri", "asdf",
-                new Cast("Rippikoulu", new ArrayList<String>()), 2000){};
+        movieManager.entryList.get(1).entryMovie = new Movie("yokeri", 1234, "asdf",
+                new Cast("Rippikoulu", new ArrayList<>()), 2000){};
         movieManager.printEntries();
         // Luokkien luomisen testaukseen
     }
@@ -35,21 +42,22 @@ public class MainActivity extends AppCompatActivity { // asdf
 
     public class MovieManager {   // MovieManager class contains the list for all movie entries
         private ArrayList<Entry> entryList;
+        private ArrayList<Movie> movieList; // lists all currently available movies
 
         public MovieManager() {
-            this.entryList = new ArrayList<Entry>();
+            this.entryList = new ArrayList<>();
+            this.movieList = new ArrayList<>();
         }
+
+
         public void printEntries() { // gettien testaamistarkoitukseen
             for (int i=0; i<2; i++) {
-                String date = entryList.get(i).getEntryDate();
-                String time = entryList.get(i).getEntryTime();
                 String location = entryList.get(i).getEntryLocation();
                 String name = entryList.get(i).entryMovie.getMovieName();
                 String desc = entryList.get(i).entryMovie.getMovieDescription();
                 String length = entryList.get(i).entryMovie.getMovieLength().toString();
 
 
-                System.out.println("Movie date and time: " + date + " " + time);
                 System.out.println("Movie location: " + location);
                 System.out.println("Movie name: " + name + "\n" + "Decription: " + desc);
                 System.out.println("Movie length: " + length + "\n");
@@ -61,44 +69,121 @@ public class MainActivity extends AppCompatActivity { // asdf
                 System.out.println("\n");
             }
         }
+        public Movie checkIfMovieExists(int newMovieID) {  // This method goes through entries and checks if a
+            // movie object already exists
+            for (int i = 0;i < this.entryList.size();i++) {
+                int movieID = this.entryList.get(i).entryMovie.getMovieID();
+                if (movieID == newMovieID) {
+                    return this.entryList.get(i).getEntryMovie();
+                }
+            }
+            return null;
+        }
+        public void getMovies() { // Generate a list of all currently available movies
+            try {
+                DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                String url = "https://www.finnkino.fi/xml/Events/";
+                Document doc = builder.parse(url);
+                doc.getDocumentElement().normalize();
+                NodeList nList = doc.getDocumentElement().getElementsByTagName("Event");
 
+                for (int n = 0; n < nList.getLength(); n++) {
+                    Movie newMovie = new Movie();
+                    Node node = nList.item(n);
+                    Element element = (Element) node;
+
+                    newMovie.setMovieID(Integer.parseInt(element.getElementsByTagName("ID").item(0).getTextContent()));
+                    newMovie.setMovieName(element.getElementsByTagName("Title").item(0).getTextContent());
+                    newMovie.setMovieLength(Integer.parseInt(element.getElementsByTagName("LengthInMinutes").item(0).getTextContent()));
+                    newMovie.setMovieDescription(element.getElementsByTagName("Synopsis").item(0).getTextContent());
+                    newMovie.movieCast = new Cast(
+                            element.getElementsByTagName("Director").item(0).getTextContent();
+                    );
+                }
+                NodeList nList = doc.getDocumentElement().getElementsByTagName("Cast");
+
+            } catch (IOException | SAXException | ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+
+        }
+        public void getEntries() {  // Gets the entries from Finnkino xml and saves them.
+
+            // List containing all the theater ids to go through
+            int[] allTheatersId = {1014,1015,1016,1017,1018,1019,1021,1022,1041};
+
+            try {
+                for (int i = 0; i<9;i++) {
+                    DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                    String url = "https://www.finnkino.fi/xml/Schedule/?area=" + allTheatersId[i] + "&nrOfDays=31";
+                    Document doc = builder.parse(url);
+                    doc.getDocumentElement().normalize();
+                    NodeList nList = doc.getDocumentElement().getElementsByTagName("Show");
+
+                    for (int n = 0; n < nList.getLength(); n++) {
+                        Node node = nList.item(n);
+                        Element element = (Element) node;
+
+                        int movieID = Integer.parseInt(element.getElementsByTagName("EventID").item(0).getTextContent());
+                        String location = element.getElementsByTagName("Theatre").item(0).getTextContent();
+                        int lengthInMin = Integer.parseInt(element.getElementsByTagName("LengthInMinutes").item(0).getTextContent());
+                        String[] dateTimeString = element.getElementsByTagName("dttmShowStart").item(0).getTextContent().split("T", 2);
+                        String[] dateString = dateTimeString[0].split("-",3);
+                        String[] timeString = dateTimeString[1].split(":",3);
+                        Date date = new Date(Integer.parseInt(dateString[0]), Integer.parseInt(dateString[1]), Integer.parseInt(dateString[2]));
+                        Time time = new Time(Integer.parseInt(timeString[0]), Integer.parseInt(timeString[1]), Integer.parseInt(timeString[2]));
+                        Movie newMovie = this.checkIfMovieExists(movieID);
+                        if (newMovie  == null) {
+                            String name = element.getElementsByTagName("Title").item(0).getTextContent();
+                        } else {
+
+                        }
+                    }
+                }
+            } catch (IOException | SAXException | ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
     }
     public class Entry { // each entry is fetched from Finkino xml and contains info on one
-        // näytös :D of a movie. Each entry added to entryList to contain information
+        // näytös :D of a movie.
         // some näytökset may contain the same movie so more entry class than movie class
         public Entry(String entryTime, String entryDate) {
             this.entryMovie = null;
-            this.entryTime = entryTime;
-            this.entryDate = entryDate;
+            this.entryTime = new Time(0,0,0);
+            this.entryDate = new Date(0,0,0);
             this.entryLocation = "lappeen Ranta";
 
         }
         private Movie entryMovie;
-        private String entryDate;
-        private String entryTime;  // TODO muuta time ja date olioiksi
+        private Date entryDate;
+        private Time entryTime;
         private String entryLocation;
 
-        private String getEntryDate() {
+        private Movie getEntryMovie() { return this.entryMovie; }
+        private Date getEntryDate() {
             return this.entryDate;
         }
         private String getEntryLocation() {
             return this.entryLocation;
         }
-        private String getEntryTime() {
+        private Time getEntryTime() {
             return this.entryTime;
         }
     }
     public class Movie {   // Movie class contains information on one unique movie
 
         // Constructor with parameters for movie info
-        public Movie(String movieName, String movieDescription, Cast movieCast, int movieLength) {
+        public Movie(String movieName, int movieID, String movieDescription, Cast movieCast, int movieLength) {
             this.movieName = movieName;
+            this.movieID = movieID;
             this.movieDescription = movieDescription;
             this.movieCast = movieCast;
             this.movieLength = new Time(movieLength);
         }
         public Movie() {  // Empty constructor
             this.movieName = "empty";
+            this.movieID = 0;
             this.movieDescription = "empty";
             this.movieCast = null;
             this.movieLength = new Time(0);
@@ -106,19 +191,27 @@ public class MainActivity extends AppCompatActivity { // asdf
         private String getMovieName() {
             return this.movieName;
         }
+        private int getMovieID() { return this.movieID; }
         private String getMovieDescription() {
             return this.movieDescription;
         }
         private Cast getMovieCast() {
             return this.movieCast;
         }
-        private Time getMovieLength() {
+        private int getMovieLength() {
             return this.movieLength;
         }
 
+        private void setMovieName(String movieName) { this.movieName = movieName; }
+        private void setMovieID(int movieID) { this.movieID = movieID; }
+        private void setMovieDescription(String movieDescription) { this.movieName = movieDescription; }
+        private void setMovieCast(Cast movieCast) { this.movieCast = movieCast; }
+        private void setMovieLength(int movieLength) { this.movieLength = movieLength; }
+
         private String movieName;
+        private int movieID;
         private String movieDescription;
-        private Time movieLength;
+        private int movieLength;
         private Cast movieCast;
     }
 
