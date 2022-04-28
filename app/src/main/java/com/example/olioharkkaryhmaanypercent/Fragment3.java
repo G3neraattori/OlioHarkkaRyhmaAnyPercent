2e0d06bc4bb526670599f21411686af16b7c8221
G3neraattori <71793648+G3neraattori@users.noreply.github.com>
2022-04-28 23:57:38 +0300

    Login fragment some functionality

--- app/src/main/java/com/example/olioharkkaryhmaanypercent/Fragment3.java
+++ app/src/main/java/com/example/olioharkkaryhmaanypercent/Fragment3.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,18 +19,22 @@ import java.security.NoSuchAlgorithmException;
 public class Fragment3 extends Fragment {
 
     View view;
+    EditText username;
+    EditText salari;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_fragment3, container, false);
+
         return view;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
         UserData user = new UserData(requireContext());
         try {
             user.createUser("a", "a");
@@ -36,6 +42,24 @@ public class Fragment3 extends Fragment {
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
+        username = (EditText) getView().findViewById(R.id.username);
+        salari = (EditText) getView().findViewById(R.id.salari);
+        Button mButton = (Button) view.findViewById(R.id.kirjautumisNappi);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    login(user);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void login(UserData user) throws NoSuchAlgorithmException {
+        user.createUser(String.valueOf(username.getText()), String.valueOf(salari.getText()));
     }
 
 
