a696f022b204c1204768ec3bcd9844c72e7cca4f
G3neraattori <71793648+G3neraattori@users.noreply.github.com>
2022-04-28 22:51:23 +0300

    Login functionality for fragment 3

--- app/src/main/java/com/example/olioharkkaryhmaanypercent/Fragment3.java
+++ app/src/main/java/com/example/olioharkkaryhmaanypercent/Fragment3.java
@@ -1,5 +1,6 @@
 package com.example.olioharkkaryhmaanypercent;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +9,10 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+
+import java.security.NoSuchAlgorithmException;
 
 public class Fragment3 extends Fragment {
 
@@ -16,16 +20,24 @@ public class Fragment3 extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         view = inflater.inflate(R.layout.fragment_fragment3, container, false);
         return view;
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        TextView text = this.view.findViewById(R.id.text3);
-        text.setText("HALOO");
+        UserData user = new UserData(requireContext());
+        try {
+            user.createUser("a", "a");
+            user.validatePassword("password", "AA");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
+
 
 
 }
